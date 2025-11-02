279e11bfc0c257f331100e4489f110ccf8c5c0bc
Muhammad Idrees <irsmaker3ds@gmail.com>
2025-11-03 00:59:45 +0500

    Random Utils Update 2

--- UnityUtils/Scripts/RandomUtils.cs
+++ UnityUtils/Scripts/RandomUtils.cs
@@ -120,6 +120,31 @@ namespace UnityUtils
         }
 
         #endregion
+
+        #region Quaternion Methods
+
+        public static Quaternion Rotation2D()
+        {
+            return Quaternion.Euler(0, 0, (float)(_random.NextDouble() * 360.0));
+        }
+
+        #endregion
+
+        #region Color Methods
+
+        /// <summary>
+        /// Returns a random, vibrant color by randomizing the Hue.
+        /// Saturation and Value can be optionally constrained for better results.
+        /// </summary>
+        public static Color ColorHSV(float saturationMin = 0.5f, float saturationMax = 1.0f, float valueMin = 0.8f, float valueMax = 1.0f)
+        {
+            float hue = (float)_random.NextDouble();
+            float saturation = (float)(_random.NextDouble() * (saturationMax - saturationMin) + saturationMin);
+            float value = (float)(_random.NextDouble() * (valueMax - valueMin) + valueMin);
+            return Color.HSVToRGB(hue, saturation, value);
+        }
+
+        #endregion
     }
 
     /// <summary>
@@ -130,6 +155,66 @@ namespace UnityUtils
         private static readonly System.Random _random = new System.Random();
 
         #region Choose Extentions
+
+        /// <summary>
+        /// Randomly selects multiple unique elements from a collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static IEnumerable<T> ChooseMultiple<T>(this IEnumerable<T> source, int count)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (count < 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+            // Shuffle the entire sequence and take the first 'count' items.
+            // This is efficient and guarantees uniqueness.
+            return source.OrderBy(x => Guid.NewGuid()).Take(count);
+        }
+
+        /// <summary>
+        /// Filters a collection based on a condition and then randomly chooses one element from the result,
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="weightSelector"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static T Choose<T>(this IEnumerable<T> source, Func<T, bool> predicate, Func<T, float> weightSelector)
+        {
+            if (source == null || predicate == null || weightSelector == null)
+                throw new ArgumentNullException();
+
+            // 1. Filter the collection first
+            var filteredItems = source.Where(predicate).ToList();
+
+            if (filteredItems.Count == 0)
+                throw new InvalidOperationException("No items matched the predicate.");
+
+            // 2. Perform a weighted choice on the *filtered* list
+            float totalWeight = filteredItems.Sum(item => weightSelector(item));
+
+            if (totalWeight <= 0)
+                throw new InvalidOperationException("Total weight of filtered items must be positive.");
+
+            float randomPoint = (float)(new Random().NextDouble() * totalWeight);
+
+            foreach (var item in filteredItems)
+            {
+                float weight = weightSelector(item);
+                if (randomPoint < weight)
+                    return item;
+                randomPoint -= weight;
+            }
+
+            // Fallback in case of floating point inaccuracies
+            return filteredItems.Last();
+        }
 
         /// <summary>
         /// Filters a collection based on a condition and then randomly chooses one element from the result.
@@ -206,6 +291,30 @@ namespace UnityUtils
             }
 
             return list[_random.Next(0, list.Count)];
+        }
+
+        #endregion
+
+        #region Vector Methods
+
+        /// <summary>
+        /// Returns a random point within the bounding box.
+        /// </summary>
+        public static Vector3 GetRandomPoint(this Bounds bounds)
+        {
+            return new Vector3(
+                (float)(_random.NextDouble() * bounds.size.x + bounds.min.x),
+                (float)(_random.NextDouble() * bounds.size.y + bounds.min.y),
+                (float)(_random.NextDouble() * bounds.size.z + bounds.min.z)
+            );
+        }
+
+        /// <summary>
+        /// Returns a random point within the collider's bounds.
+        /// </summary>
+        public static Vector3 GetRandomPoint(this Collider collider)
+        {
+            return collider.bounds.GetRandomPoint();
         }
 
         #endregion
